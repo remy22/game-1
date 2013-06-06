bb952979f6088dc0bd5ea89f4e1318c534003cd6
Joshua Prendergast <j.prendo10@gmail.com>
2013-06-06 17:52:31 +0100

    Add tilemap

---
+++ Alpha/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace Game {
+    /// <summary>
+    /// A game entity.
+    /// </summary>
+    public class GameObject : Transformable {
+        private static Dictionary<string, Texture> textures = new Dictionary<string, Texture>();
+        public static Texture DefaultTexture { set; get; }
+
+        static GameObject() {
+            DefaultTexture = new Texture("images/default.png");
+        }
+
+        public void Update(World world) {
+        }
+
+        public static Texture GetTexture(string filename) {
+            if (!textures.ContainsKey(filename))
+                textures[filename] = new Texture(filename);
+            return textures[filename];
+        }
+    }
+}
---
+++ Alpha/Tile.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Game {
+    public class Tile {
+        public int ID { set; get; }
+
+        public Tile(int id) {
+            this.ID = id;
+        }
+    }
+}
